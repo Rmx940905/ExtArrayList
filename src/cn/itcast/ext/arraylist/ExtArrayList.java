db7beedcefd6b9b19c968301f71e7811d3fcb565
RMX <1454552467@qq.com>
2018-11-24 00:40:21 +0800

    完成了add方法

---
+++ src/cn/itcast/ext/arraylist/ExtArrayList.java
@@ -0,0 +1,45 @@
+package cn.itcast.ext.arraylist;
+
+/*
+    自定义ArrayList集合
+ */
+public class ExtArrayList {
+
+    //ArrayList底层采用数组存放
+    private Object [] elementData;
+
+    //默认数组容量
+    private static final int DEFAULT_CAPACITY = 10;
+
+    //记录实际ArrayList大小
+    private int size;
+
+    //ArrayList指定数组初始的容量
+    public ExtArrayList(int initialCapacity){
+        if(initialCapacity < 0){
+            throw new IllegalArgumentException("初始容量不能小于0");
+        }
+        elementData = new Object[initialCapacity];
+    }
+
+    //默认初始化容量为10
+    public ExtArrayList(){
+        this(DEFAULT_CAPACITY);
+    }
+
+    public void add(Object object){
+        //1、判断实际存放的数据容量是否大于elementData
+        if (size == elementData.length){
+            //新数组容量大小
+            int newCapaCity = 2 * size;
+            //将老数组的值复制到新数组里面去
+            Object[] newObjects = new Object[newCapaCity];
+            for(int i = 0; i < elementData.length; i++){
+                newObjects[i] = elementData[i];
+            }
+            elementData = newObjects;
+        }
+        //2、使用下标进行赋值
+        elementData[size++] = object;
+    }
+}
---
+++ src/cn/itcast/test/Test001.java
@@ -0,0 +1,25 @@
+package cn.itcast.test;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import java.util.Arrays;
+
+public class Test001 {
+
+
+    //底层采用数组方式
+    //如可保证集合存放无限大小---数组扩容技术
+    public static void main(String[] args) {
+        Object[] objects =  {1,2};
+        System.out.println(objects.length);
+        //返回的数组,将原来长度为2的数组扩容到长度为10,原来本身的数据不变;
+        Object[] copyNewObject = Arrays.copyOf(objects,10);
+        System.out.println(copyNewObject.length);
+
+        //参数 src 原数组, srcPos 起始位置, dest 目标数组,  destPos 目标起始位置 length 复制长度
+        //System.arraycopy(src,srcPos,dest,destPos,length);
+        int[] fun = {0,1,2,3,4,5,6,7,8,9};
+        System.arraycopy(fun,0,fun,3,3);
+        System.out.println( Arrays.toString(fun));
+    }
+}
